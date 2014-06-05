2f6daa6f4627b6abdb8498b3356d2eff3b33e833
lzprgmr <baiyanhuang@gmail.com>
2014-06-06 06:52:40 +0800

    Design urls, and implment index by retriving data from database

--- arena/django/mysite/polls/views.py
+++ arena/django/mysite/polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello, world. You'are at the poll index")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
+
+def detail(request, poll_id):
+    return HttpResponse("Youre looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    raise Http404()
+    #return HttpResponse("You're voting on poll %s" % poll_id)
