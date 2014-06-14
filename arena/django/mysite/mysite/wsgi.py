3605b5fb1626b5675140f4dbf57ec90d9a749b08
lzprgmr <baiyanhuang@gmail.com>
2014-06-14 13:27:00 +0800

    Add mysite's path in order to load mysite.settings

--- arena/django/mysite/mysite/wsgi.py
+++ arena/django/mysite/mysite/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append(os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
