d6b3f9dda3454c1678ee7652c491e6a4656a996c
lzprgmr <baiyanhuang@gmail.com>
2014-06-19 07:13:22 +0800

    Add mysql connection

--- arena/django/mysite/mysite/settings.py
+++ arena/django/mysite/mysite/settings.py
@@ -55,13 +55,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'mysite', 'mysql.cnf')
+        }
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
