cf74b8c07ea5c489cf5de73e0a374bf16aadf89a
lzprgmr <baiyanhuang@gmail.com>
2014-07-02 06:23:27 +0800

    Add a bash script shows how to process command line argument

---
+++ arena/bash/simplecmdlineparser.sh
@@ -0,0 +1,19 @@
+
+echo "=>The whole command line is: " "$@"
+
+echo "=>The options are: "
+for option in "$@" # options must come at the very first, then positional arguments
+do
+	if [[ $option == --* ]]
+	then
+		expr=${option:2} # ${parameter:offset:length} to get substring
+		echo $expr
+		eval $expr # --myoption won't work, we need an assignment
+		shift
+	fi	
+done
+
+echo "=>The captured variables for options: "
+set | tail -3
+
+echo "=>the positional arguments are: " "$@"
