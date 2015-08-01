9c8c64b11d2dba8a83ae33ace610793d1ec17bd5
baiyanhuang <baiyanhuang@gmail.com>
2015-08-01 12:51:28 +0800

    Add python3 alias

--- config/batch_profile.cmd
+++ config/batch_profile.cmd
@@ -1,3 +1,4 @@
 REM bash
 REM windows system tools
-doskey sublime="C:\Program Files\Sublime Text 2\sublime_text.exe" %*
+doskey sublime="C:\Program Files\Sublime Text 2\sublime_text.exe" $*
+doskey python3=C:\Python34\python.exe $*
