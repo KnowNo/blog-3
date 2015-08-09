09187836096290471afff5eb04e65b0c95443c7d
baiyanhuang <baiyanhuang@gmail.com>
2015-08-09 16:49:27 +0800

    Update windows dev environment configuration

--- config/batch_profile.cmd
+++ config/batch_profile.cmd
@@ -1,4 +1,12 @@
-REM bash
-REM windows system tools
+@echo off
+
+REM TODO: bash
+REM TODO: windows system tools
+
+
+REM alias
 doskey sublime="C:\Program Files\Sublime Text 2\sublime_text.exe" $*
 doskey python3=C:\Python34\python.exe $*
+
+REM path
+set PATH=C:\Program Files\Java\jdk1.8.0_31\bin;%PATH%
